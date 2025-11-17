fbbc23fe44ca02d42f65a642d86c2cfb6d513aa0
caoyangjie <caoyjf@dcits.com>
2025-11-17 21:19:20 +0800

    访问后端增加 上下文

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     cors: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:18080',
+        target: 'http://localhost:18080/gameweb',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
